de8ed3f838c176f57b3e7ad879f40af9da07f4ff
Krupa <krupagawade@yahoo.com>
2018-07-29 15:39:03 -0500

    mLab changes

--- server.js
+++ server.js
@@ -28,7 +28,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/websiteScrapeMongoDB");
+//mongoose.connect("mongodb://localhost/websiteScrapeMongoDB");
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/websiteScrapeMongoDB";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
